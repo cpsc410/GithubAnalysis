a0e4d0178aba9f40e3cc8c617bf8b56972f67e9b
Ceylin Ozdemir <ceylin@dhcp-128-189-92-113.ubcsecure.wireless.ubc.ca>
2019-11-16 15:14:24 -0800

    Saves the output to a stats.txt file

--- gitStats.sh
+++ gitStats.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Number of commits for each author: "
-git shortlog --numbered --summary --no-merges
+# params to change
+lang="java" #primary language extention used in repo
+
+echo "Number of commits for each author: " > stats.txt  
+git shortlog --numbered --summary --no-merges >> stats.txt 
 
 authorInfo=$(git log --pretty="%an" | sort | uniq)
 authors=$(echo "$authorInfo" | cut -d ' ' -f 1)
@@ -9,34 +12,35 @@ authors=$(echo "$authorInfo" | cut -d ' ' -f 1)
 
 for author in $authors
 do
-    echo -e "\nAuthor: $author"
-    echo "Number of lines"
-    echo "Added | Deleted | File  Modified"
+    echo -e "\nAuthor: $author" >> stats.txt  
+    echo "Number of lines" >> stats.txt 
+    echo "Added | Deleted | File  Modified" >> stats.txt 
     stats=$(git log --author="q6y0b" --pretty=tformat: --numstat | grep -v '^-')
-    echo "${stats}"
-    echo "Total:"
-    git log --author="$author" --pretty=tformat: --numstat | grep -v '^-' | awk '{ add+=$1; remove+=$2 } END { print add, remove }' 
+    echo "${stats}" >> stats.txt 
+    echo "Total:" >> stats.txt 
+    git log --author="$author" --pretty=tformat: --numstat | grep -v '^-' | awk '{ add+=$1; remove+=$2 } END { print add, remove }' >> stats.txt 
 
-    echo -e "\nList of unique files modified by $author: "
+    echo -e "\nList of unique files modified by $author: " >> stats.txt 
     modifiedFiles=()
 	# regex for number
 	re='^[0-9]+$'
 	for s in $stats
 	do
 		# check if s is not a number, ends in java and is not already in list modifiedFiles
-		if ! [[ $s =~ $re ]] && [[ "${s##*.}" == "java" ]] && [[ ! " ${modifiedFiles[@]} " =~ " ${s} " ]]; then
+		if ! [[ $s =~ $re ]] && [[ "${s##*.}" == "$lang" ]] && [[ ! " ${modifiedFiles[@]} " =~ " ${s} " ]]; then
 			modifiedFiles+=( "$s" )
 		fi
 	done
 
-	echo "${modifiedFiles[@]}"
+	# echo "${modifiedFiles[@]}"
+	printf '%s\n' "${modifiedFiles[@]}" >> stats.txt 
 
 
 done
 
 codeFiles=$(find . -name "*.java")
-echo -e "\nAll the code files: "
-echo "$codeFiles"
+echo -e "\nAll the code files: " >> stats.txt 
+echo "$codeFiles" >> stats.txt 
 
 
 
