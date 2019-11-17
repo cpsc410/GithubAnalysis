dce0b359f893a15e695f725f9e0e3063b39424dc
Ceylin Ozdemir <ceylin@Ceylins-MBP.hitronhub.home>
2019-11-17 15:31:10 -0800

    Adding the txt file to be parsed

--- gitStats.sh
+++ gitStats.sh
@@ -35,28 +35,6 @@ do
 	# echo "${modifiedFiles[@]}"
 	printf '%s\n' "${modifiedFiles[@]}" >> stats.txt 
 
-    echo -e "\nAuthor: $author"
-    echo "Number of lines"
-    echo "Added | Deleted | File  Modified"
-    stats=$(git log --author="q6y0b" --pretty=tformat: --numstat | grep -v '^-')
-    echo "${stats}"
-    echo "Total:"
-    git log --author="$author" --pretty=tformat: --numstat | grep -v '^-' | awk '{ add+=$1; remove+=$2 } END { print add, remove }' 
-
-    echo -e "\nList of unique files modified by $author: "
-    modifiedFiles=()
-	# regex for number
-	re='^[0-9]+$'
-	for s in $stats
-	do
-		# check if s is not a number, ends in java and is not already in list modifiedFiles
-		if ! [[ $s =~ $re ]] && [[ "${s##*.}" == "java" ]] && [[ ! " ${modifiedFiles[@]} " =~ " ${s} " ]]; then
-			modifiedFiles+=( "$s" )
-		fi
-	done
-
-	echo "${modifiedFiles[@]}"
-
 
 done
 
@@ -65,3 +43,22 @@ echo -e "\nAll the code files: " >> stats.txt
 echo "$codeFiles" >> stats.txt 
 
 
+
+# Testing
+# echo "For aurhor q6y0b"
+
+# stats=$(git log --author="q6y0b" --pretty=tformat: --numstat | grep -v '^-')
+# # echo $stats
+# modifiedFiles=()
+# # regex for number
+# re='^[0-9]+$'
+# for s in $stats
+# do
+# 	# check if s is not a number, ends in java and is not already in list modifiedFiles
+# 	if ! [[ $s =~ $re ]] && [[ "${s##*.}" == "java" ]] && [[ ! " ${modifiedFiles[@]} " =~ " ${s} " ]]; then
+# 		modifiedFiles+=( "$s" )
+	 
+# 	fi
+# done
+
+# echo "${modifiedFiles[@]}"
