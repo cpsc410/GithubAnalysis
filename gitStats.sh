fccc9cdec7858979fd55afb3594a3bae527acf58
Ceylin Ozdemir <ceylin@Ceylins-MBP.hitronhub.home>
2019-11-17 16:02:42 -0800

    Fixed the files committed by each author

--- gitStats.sh
+++ gitStats.sh
@@ -3,8 +3,12 @@
 # params to change
 lang="java" #primary language extention used in repo
 
-echo "Number of commits for each author: " > stats.txt  
-git shortlog --numbered --summary --no-merges >> stats.txt 
+codeFiles=$(find . -name "*.java")
+echo -e "\nAll the code files: " > stats.txt 
+echo "$codeFiles" >> stats.txt 
+
+echo -e "\nNumber of commits for each author: " >> stats.txt  
+git shortlog --numbered --summary --no-merges >> stats.txt
 
 authorInfo=$(git log --pretty="%an" | sort | uniq)
 authors=$(echo "$authorInfo" | cut -d ' ' -f 1)
@@ -15,11 +19,11 @@ do
     echo -e "\nAuthor: $author" >> stats.txt  
     echo "Number of lines" >> stats.txt 
     echo "Added | Deleted | File  Modified" >> stats.txt 
-    stats=$(git log --author="q6y0b" --pretty=tformat: --numstat | grep -v '^-')
+    stats=$(git log --author="$author" --pretty=tformat: --numstat | grep -v '^-')
     echo "${stats}" >> stats.txt 
     echo "Total:" >> stats.txt 
     git log --author="$author" --pretty=tformat: --numstat | grep -v '^-' | awk '{ add+=$1; remove+=$2 } END { print add, remove }' >> stats.txt 
-
+ 
     echo -e "\nList of unique files modified by $author: " >> stats.txt 
     modifiedFiles=()
 	# regex for number
@@ -34,31 +38,4 @@ do
 
 	# echo "${modifiedFiles[@]}"
 	printf '%s\n' "${modifiedFiles[@]}" >> stats.txt 
-
-
 done
-
-codeFiles=$(find . -name "*.java")
-echo -e "\nAll the code files: " >> stats.txt 
-echo "$codeFiles" >> stats.txt 
-
-
-
-# Testing
-# echo "For aurhor q6y0b"
-
-# stats=$(git log --author="q6y0b" --pretty=tformat: --numstat | grep -v '^-')
-# # echo $stats
-# modifiedFiles=()
-# # regex for number
-# re='^[0-9]+$'
-# for s in $stats
-# do
-# 	# check if s is not a number, ends in java and is not already in list modifiedFiles
-# 	if ! [[ $s =~ $re ]] && [[ "${s##*.}" == "java" ]] && [[ ! " ${modifiedFiles[@]} " =~ " ${s} " ]]; then
-# 		modifiedFiles+=( "$s" )
-	 
-# 	fi
-# done
-
-# echo "${modifiedFiles[@]}"
