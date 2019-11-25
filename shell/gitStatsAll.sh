822c535af64742b5bec201c3776017523a526610
b3x0b <b3x0b@ugrad.cs.ubc.ca>
2019-11-25 11:37:54 -0800

    updated

--- shell/gitStatsAll.sh
+++ shell/gitStatsAll.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # Usage: ./gitallStats.sh <stats.txt>
-
 outputName="$1"
+
 # echo "$outputName"
 # params to change
 currentBranch="master" #primary language extention used in repo
-
-echo -e "\nNumber of commits for each author: " > "$outputName"  
-git shortlog --numbered --summary --no-merges >> "$outputName"
+echo -e "\nNumber of commits for each author: " > "$outputName"
+echo "" | git shortlog --numbered --summary --no-merges >> "$outputName"
 
 codeFiles=$(git ls-tree -r "$currentBranch" --name-only)
-echo -e "\nAll the code files: " >> "$outputName" 
-echo "$codeFiles" >> "$outputName" 
+echo -e "\nAll the code files: " >> "$outputName"
+echo "$codeFiles" >> "$outputName"
 
 
 authorInfo=$(git log --pretty="%an" | sort | uniq)
@@ -20,15 +19,15 @@ authors=$(echo "$authorInfo" | cut -d ' ' -f 1)
 
 for author in $authors
 do
-    echo -e "\nAuthor: $author" >> "$outputName"  
-    # echo "Number of lines" >> "$outputName" 
-    # echo "Added | Deleted | File  Modified" >> "$outputName" 
+    echo -e "\nAuthor: $author" >> "$outputName"
+    # echo "Number of lines" >> "$outputName"
+    # echo "Added | Deleted | File  Modified" >> "$outputName"
     allStats=$(git log --author="$author" --pretty=tformat: --numstat | grep -v '^-')
-    echo "${allStats}" >> "$outputName" 
-    echo "Total:" >> "$outputName" 
-    git log --author="$author" --pretty=tformat: --numstat | grep -v '^-' | awk '{ add+=$1; remove+=$2 } END { print add, remove }' >> "$outputName" 
- 
-    # echo -e "\nList of unique files modified by $author: " >> "$outputName" 
+    echo "${allStats}" >> "$outputName"
+    echo "Total:" >> "$outputName"
+    git log --author="$author" --pretty=tformat: --numstat | grep -v '^-' | awk '{ add+=$1; remove+=$2 } END { print add, remove }' >> "$outputName"
+
+    # echo -e "\nList of unique files modified by $author: " >> "$outputName"
     # modifiedFiles=()
 	# # regex for number
 	# re='^[0-9]+$'
@@ -41,5 +40,5 @@ do
 	# done
 
 	# echo "${modifiedFiles[@]}"
-	# printf '%s\n' "${modifiedFiles[@]}" >> "$outputName" 
+	# printf '%s\n' "${modifiedFiles[@]}" >> "$outputName"
 done
